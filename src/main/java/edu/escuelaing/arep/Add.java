d9694cd3c330de9d1192ed01d1c357fcf90f9c59
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-04-17 16:00:53 -0500

    esqueleto post

---
+++ src/main/java/edu/escuelaing/arep/Add.java
@@ -0,0 +1,15 @@
+package edu.escuelaing.arep;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Add {
+
+    public String enviarMensaje (String nuevapost){
+
+        return "";
+    }
+
+}
--- src/main/java/edu/escuelaing/arep/App.java
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -1,5 +1,9 @@
 package edu.escuelaing.arep;
 
+
+import static spark.Spark.*;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +12,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        port(getPort());
+        get("/hello", (req, res) -> "Hello post");
+
+        post("/post", (request, response) -> {
+            System.out.println("entro a postear");
+            response.type("application/json");
+            return (request.queryParams("value"));
+        });
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set(i.e. on localhost)
     }
 }
+
